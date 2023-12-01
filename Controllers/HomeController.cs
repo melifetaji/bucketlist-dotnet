d05d3c8058fb3e64027533781f57d81cf4664f72
Meli <melfetaji@gmail.com>
2023-12-01 15:25:02 +0100

    home views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return RedirectToAction("Index", "BucketItem");
+        return View();
     }
 
     public IActionResult Privacy()
